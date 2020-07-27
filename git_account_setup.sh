bba2f3a98e6d7d022f5863117d7476fe47281d0b
sid597 <siddharthdv77@gmail.com>
2020-07-27 21:52:21 +0000

    2 new scripts 1 for setting up github account and another for getting ssl certificate

---
+++ git_account_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git config --global user.email "siddharthdv77@gmail.com"
+git config --global user.name "sid597"
+
---
+++ ssl_certificate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This is for ubuntu 18
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+
+sudo apt-get install certbot python3-certbot-nginx
+
+sudo certbot --nginx
